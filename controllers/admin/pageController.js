781262f071d9ce33cccde5722c250fc523f0bb78
Furkan Sade Uckun <88970339+furkansade@users.noreply.github.com>
2023-03-16 13:54:43 +0300

    finished-admin-page-route

--- controllers/admin/pageController.js
+++ controllers/admin/pageController.js
@@ -1,3 +1,25 @@
 exports.getProfilePage = async (req, res) => {
-    res.status(200).render("admin/profile")
-}
+  res.status(200).render("admin/profile", {
+    pageName: "profile",
+  });
+};
+exports.getAppointmentsPage = async (req, res) => {
+  res.status(200).render("admin/appointments", {
+    pageName: "appointments",
+  });
+};
+exports.getDepartmentsPage = async (req, res) => {
+  res.status(200).render("admin/deparments", {
+    pageName: "deparments",
+  });
+};
+exports.getDoctorsPage = async (req, res) => {
+  res.status(200).render("admin/doctors", {
+    pageName: "doctors",
+  });
+};
+exports.getFaqsPage = async (req, res) => {
+  res.status(200).render("admin/faqs", {
+    pageName: "faqs",
+  });
+};
--- routes/admin/pageRoute.js
+++ routes/admin/pageRoute.js
@@ -4,5 +4,9 @@ const pageController = require("../../controllers/admin/pageController");
 const router = express.Router();
 
 router.route("/profile").get(pageController.getProfilePage);
+router.route("/appointments").get(pageController.getAppointmentsPage);
+router.route("/departments").get(pageController.getDepartmentsPage);
+router.route("/doctors").get(pageController.getDoctorsPage);
+router.route("/faqs").get(pageController.getFaqsPage);
 
 module.exports = router;
