8b56d0f331f51fef91c0cefcb65787aa5362ccfb
Furkan Sade Uckun <88970339+furkansade@users.noreply.github.com>
2023-03-24 18:02:00 +0300

    added-faqs

--- controllers/site/pageController.js
+++ controllers/site/pageController.js
@@ -1,14 +1,18 @@
 const Doctor = require("../../models/Doctor");
 const Department = require("../../models/Department");
+const FAQ = require("../../models/FAQ");
 
 exports.getHomePage = async (req, res) => {
   const doctors = await Doctor.find().populate("department");
   const departments = await Department.find();
+  const faqs = await FAQ.find();
+
 
   res.status(200).render("site/index", {
     pageName: "home",
     doctors,
     departments,
+    faqs
   });
 };
 
